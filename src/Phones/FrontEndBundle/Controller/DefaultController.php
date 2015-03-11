40c4f1322f6bbf17b9738456393eb2f8bc75f5f1
Mazvis <sivzam@gmail.com>
2015-03-11 12:33:58 +0200

    main filters with values created

--- src/Phones/FrontEndBundle/Controller/DefaultController.php
+++ src/Phones/FrontEndBundle/Controller/DefaultController.php
@@ -2,13 +2,18 @@
 
 namespace Phones\FrontEndBundle\Controller;
 
+use Phones\PhoneBundle\Entity\Phone;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     *
+     * @return Response
      */
     public function indexAction()
     {
@@ -18,65 +23,49 @@ class DefaultController extends Controller
         return $this->render('PhonesFrontEndBundle:Default:index.html.twig', $params);
     }
 
+    /**
+     * @return Response
+     */
     public function bestPhoneSearchAction()
     {
         $params =[];
         $params['products'] = $this->getPhones();
-
-        // create a task and give it some dummy data for this example
-//        $task = new Task();
-//        $task->setTask('Write a blog post');
-//        $task->setDueDate(new \DateTime('tomorrow'));
-
-        $rowOptions = [
-            'attr' => [
-                'class' => 'form-control'
-            ],
-            'label_attr' => [
-//                'class' => 'col-sm-2 control-label'
-                'class' => 'control-label'
-            ],
-//            'placeholder' => 'type sth'
-        ];
-        $selectOptions = [
-            'label'=> 'Paieska',
-            'attr' => [
-                'class' => 'btn btn-success'
-            ]
-        ];
-        $formOptions = [
-            'attr' => [
-                'class' => 'form-horizontal'
-            ]
-        ];
-
-        $form = $this->createFormBuilder(null, $formOptions)
-            ->add('brand', 'text', $rowOptions) //select and checkbox(multi select)
-            ->add('cost', 'text', $rowOptions) //range
-            ->add('weight', 'text', $rowOptions) //range
-            ->add('os', 'text', $rowOptions) //select and checkbox(multi select)
-            ->add('cpu_freq', 'text', $rowOptions) //range
-            ->add('cpu_cores', 'text', $rowOptions) //range
-            ->add('ram', 'text', $rowOptions) //range
-            ->add('external_sd', 'checkbox', $rowOptions) //bool
-            ->add('display_size', 'text', $rowOptions) //range
-            ->add('display_resolution', 'text', $rowOptions) //range
-            ->add('camera_mpx', 'text', $rowOptions) //range
-            ->add('video_p', 'text', $rowOptions) //range? gal isvis nereik
-            ->add('flash', 'checkbox', $rowOptions) //bool
-            ->add('technology', 'text', $rowOptions) //select and checkbox(multi select)
-            ->add('gps', 'text', $rowOptions) //select and checkbox(multi select)
-            ->add('wlan', 'text', $rowOptions) //select and checkbox(multi select)
-            ->add('bluetoot_version', 'text', $rowOptions) //select and checkbox(multi select)
-            ->add('battery_stand_by', 'text', $rowOptions) //range
-            ->add('battery_talk_time', 'text', $rowOptions) //range
-
-            ->add('submit', 'submit', $selectOptions)
-            ->getForm();
-
-        $params['form'] = $form->createView();
+        $params['distinctOs'] = $this->getExistingOs();
+        $params['distinctBrands'] = $this->getExistingBrands();
+        $params['distinctBlueTooth'] = $this->getExistingBlueToothVersions();
 
         return $this->render('PhonesFrontEndBundle:Default:best.phone.search.html.twig', $params);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function searchResultsAction(Request $request){
+        $brand             = $request->query->get('brand');
+        $cost              = $request->query->get('cost');
+        $weight            = $request->query->get('weight');
+        $os                = $request->query->get('os');
+        $cpu_freq          = $request->query->get('cpu_freq');
+        $cpu_cores         = $request->query->get('cpu_cores');
+        $ram               = $request->query->get('ram');
+        $external_sd       = $request->query->get('external_sd');
+        $display_size      = $request->query->get('display_size');
+        $camera_mpx        = $request->query->get('camera_mpx');
+        $video_p           = $request->query->get('video_p');
+        $flash             = $request->query->get('flash');
+        $technology        = $request->query->get('technology');
+        $gps               = $request->query->get('gps');
+        $wlan              = $request->query->get('wlan');
+        $bluetooth_version = $request->query->get('bluetooth_version');
+        $battery_stand_by  = $request->query->get('battery_stand_by');
+        $battery_talk_time = $request->query->get('battery_talk_time');
+
+        $params = [];
+        $params['results'] = $cost;
+
+        return $this->render('PhonesFrontEndBundle:Default:results.html.twig', $params);
     }
 
     /**
@@ -89,5 +78,47 @@ class DefaultController extends Controller
             ->findAll();
 
         return $products;
+    }
+
+    public function getExistingOs()
+    {
+        $qb = $this->getDoctrine()->getRepository('PhonesPhoneBundle:Phone')->createQueryBuilder('a')->groupBy('a.os');
+        $result = $qb->getQuery()->getResult();
+
+        $distinctOs = [];
+        /** @var Phone $os */
+        foreach($result as $os) {
+            $distinctOs[] = $os->getOs();
+        }
+
+        return $distinctOs;
+    }
+
+    public function getExistingBrands()
+    {
+        $qb = $this->getDoctrine()->getRepository('PhonesPhoneBundle:Phone')->createQueryBuilder('a')->groupBy('a.brand');
+        $result = $qb->getQuery()->getResult();
+
+        $distinct = [];
+        /** @var Phone $os */
+        foreach($result as $os) {
+            $distinct[] = $os->getBrand();
+        }
+
+        return $distinct;
+    }
+
+    public function getExistingBlueToothVersions()
+    {
+        $qb = $this->getDoctrine()->getRepository('PhonesPhoneBundle:Phone')->createQueryBuilder('a')->groupBy('a.bluetooth_version');
+        $result = $qb->getQuery()->getResult();
+
+        $distinct = [];
+        /** @var Phone $os */
+        foreach($result as $os) {
+            $distinct[] = $os->getBluetoothVersion();
+        }
+
+        return $distinct;
     }
 }
