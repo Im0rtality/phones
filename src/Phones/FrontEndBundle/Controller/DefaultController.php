8361e4cc34d97ac137a450c7949fe1f8648859f5
Mazvis <sivzam@gmail.com>
2015-03-21 22:00:11 +0200

    some logic moved to another class as query helper service

--- src/Phones/FrontEndBundle/Controller/DefaultController.php
+++ src/Phones/FrontEndBundle/Controller/DefaultController.php
@@ -2,10 +2,7 @@
 
 namespace Phones\FrontEndBundle\Controller;
 
-use Doctrine\Common\Persistence\ObjectRepository;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Query\Expr;
-use Phones\PhoneBundle\Entity\Phone;
+use Phones\FrontEndBundle\Service\QueryHelper;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,6 +11,14 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller
 {
     /**
+     * @return QueryHelper
+     */
+    public function getQueryHelper()
+    {
+        return $this->get('phones_front_end.query_helper');
+    }
+
+    /**
      * @Route("/", name="homepage")
      *
      * @return Response
@@ -21,7 +26,7 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $params =[];
-        $params['products'] = $this->getPhones();
+        $params['products'] = $this->getQueryHelper()->getPhones();
 
         return $this->render('PhonesFrontEndBundle:Default:index.html.twig', $params);
     }
@@ -32,9 +37,9 @@ class DefaultController extends Controller
     public function bestPhoneSearchAction()
     {
         $params =[];
-        $params['products'] = $this->getPhones();
-        $params['distinctOs'] = $this->getExistingOs();
-        $params['distinctBrands'] = $this->getExistingBrands();
+        $params['products'] = $this->getQueryHelper()->getPhones();
+        $params['distinctOs'] = $this->getQueryHelper()->getExistingOs();
+        $params['distinctBrands'] = $this->getQueryHelper()->getExistingBrands();
 
         return $this->render('PhonesFrontEndBundle:Default:best.phone.search.html.twig', $params);
     }
@@ -45,251 +50,10 @@ class DefaultController extends Controller
      * @return Response
      */
     public function searchResultsAction(Request $request){
-        $possibleRangeNames = [
-            'cost' => 'costs',
-            'weight' => 'phones',
-            'cpu_freq' => 'phones',
-            'cpu_cores' => 'phones',
-            'ram_mb' => 'phones',
-            'display_size' => 'phones',
-            'camera_mpx' => 'phones',
-            'video_p' => 'phones',
-            'battery_stand_by_h' => 'phones',
-            'battery_talk_time' => 'phones',
-        ];
-
-        $possibleSelectNames = [
-            'brand' => 'phones',
-            'os' => 'phones',
-            'bluetooth_version' => 'phones',
-        ];
-
-        $possibleLikeSelectNames = [
-            'technology' => 'phones',
-            'gps' => 'phones',
-            'wlan' => 'phones',
-        ];
-
-        $possibleCheckBoxNames = [
-            'external_sd' => 'phones',
-            'flash' => 'phones',
-        ];
-
-        $qBuilder = $this->getQueryBuilder();
-        $qBuilder->addSelect('phones.phoneId');
-        $qBuilder->addSelect('costs.cost');
-
-        $qBuilder->from('PhonesPhoneBundle:Phone','phones');
-
-        $qBuilder->leftJoin('PhonesPhoneBundle:Cost', 'costs', "WITH", 'costs.phone_id=phones.phoneId');
-
-        $queryParams = $request->query->getIterator();
-        foreach ($queryParams as $paramName => $paramValue) {
-            if (isset($possibleRangeNames[$paramName])) {
-                $params = explode(',', $paramValue);
-                $value = $possibleRangeNames[$paramName] . '.' . $paramName;
-                $this->generateRangeConditions(
-                    $qBuilder,
-                    $value . ' >= :%s AND '.$value.' <= :%s',
-                    $paramName,
-                    $params
-                );
-            } elseif (isset($possibleSelectNames[$paramName])) {
-                if (is_array($paramValue) && in_array('any', $paramValue)) {
-                    unset($paramValue[array_search('any', $paramValue)]);
-                }
-                $value = $possibleSelectNames[$paramName] . '.' . $paramName;
-                $this->generateMultipleConditions(
-                    $qBuilder,
-                    $qBuilder->expr()->orX(),
-                    $value . ' = :%s',
-                    $paramName,
-                    $paramValue
-                );
-            } elseif (isset($possibleLikeSelectNames[$paramName]))  {
-                if (is_array($paramValue) && in_array('any', $paramValue)) {
-                    unset($paramValue[array_search('any', $paramValue)]);
-                }
-                $value = $possibleLikeSelectNames[$paramName] . '.' . $paramName;
-//                $this->generatelikeConditions(
-//                    $qBuilder,
-//                    $qBuilder->expr()->orX(),
-//                    $value,
-//                    $paramName,
-//                    $paramValue
-//                );
-                foreach ($paramValue as $key => $valueStr) {
-                    $paramValue[$key] = '%'.$valueStr.'%';
-                }
-                $this->generateMultipleConditions(
-                    $qBuilder,
-                    $qBuilder->expr()->orX(),
-                    $value . ' LIKE :%s',
-                    $paramName,
-                    $paramValue
-                );
-            } elseif (isset($possibleCheckBoxNames[$paramName])) {
-                $value = $possibleCheckBoxNames[$paramName] . '.' . $paramName;
-                $this->generateMultipleConditions(
-                    $qBuilder,
-                    $qBuilder->expr()->orX(),
-                    $value . ' = :%s',
-                    $paramName,
-                    [1]
-                );
-            }
-//            var_dump($key, $param);
-        }
-
-        $query = $qBuilder->getQuery();
-        var_dump($query->getResult());
-
+        $phones = $this->getQueryHelper()->getBestPhones($request);
+        var_dump($phones);
         $params = [];
 
-//        var_dump($this->getFilteredPhones(['bluetooth_version' > '2']));
-//        var_dump($this->test());
-
         return $this->render('PhonesFrontEndBundle:Default:results.html.twig', $params);
-    }
-
-    private function test()
-    {
-        $qb = $this->getQueryBuilder();
-
-        $qb->addSelect('phones.phoneId');
-        $qb->addSelect('costs.cost');
-
-        $qb->from('PhonesPhoneBundle:Phone','phones');
-
-        $qb->leftJoin('PhonesPhoneBundle:Cost', 'costs', "WITH", 'costs.phone_id=phones.phoneId');
-
-        $this->generateMultipleConditions($qb, $qb->expr()->orX(), 'phones.brand = :%s', 'brand', [
-            'Nokia',
-            'Apple',
-            'HTC'
-        ]);
-        $this->generateRangeConditions($qb, 'costs.cost >= :%s AND costs.cost <= :%s', 'cost', [500, 1000]);
-        $this->generateMultipleConditions($qb, $qb->expr()->orX(), 'phones.os = :%s', 'os', [
-            'iOS',
-            'Microsoft Windows Phone',
-            'Android'
-        ]);
-
-        $query = $qb->getQuery();
-        return $query->getResult();
-        return $query;
-    }
-
-    /**
-     * @param \Doctrine\ORM\QueryBuilder $qBuilder
-     * @param $expr
-     * @param string $pattern
-     * @param string $fieldName
-     * @param array $values
-     */
-    private function generateMultipleConditions($qBuilder, $expr, $pattern, $fieldName, $values)
-    {
-        foreach ($values as $key => $value) {
-            $paramName = $fieldName . $key;
-            $condition = sprintf($pattern, $paramName);
-            $expr->add($condition);
-            $qBuilder->setParameter($paramName, $value);
-        }
-
-        if (!empty($values)) {
-            $qBuilder->andWhere($expr);
-        }
-    }
-
-    /**
-     * @param \Doctrine\ORM\QueryBuilder $qBuilder
-     * @param $expr
-     * @param string $pattern
-     * @param string $fieldName
-     * @param array $values
-     */
-    private function generateLikeConditions($qBuilder, $expr, $pattern, $fieldName, $values)
-    {
-        foreach ($values as $value) {
-            $paramName = 'aaa'.rand(1,5);
-            $expr->add($qBuilder->expr()->like($pattern, ':'.$paramName));
-            $qBuilder->setParameter($paramName, $value);
-        }
-
-        if (!empty($values)) {
-            $qBuilder->andWhere($expr);
-        }
-    }
-
-    /**
-     * @param \Doctrine\ORM\QueryBuilder $qBuilder
-     * @param string $pattern
-     * @param string $fieldName
-     * @param array  $values
-     */
-    private function generateRangeConditions($qBuilder, $pattern, $fieldName, $values)
-    {
-        if (!empty($values)) {
-            $paramFromName = $fieldName. 0;
-            $paramToName   = $fieldName. 1;
-            $qBuilder->setParameter($paramFromName, $values[0]);
-            $qBuilder->setParameter($paramToName, $values[1]);
-
-            $condition = sprintf($pattern, $paramFromName, $paramToName);
-            $qBuilder->andWhere($condition);
-        }
-    }
-
-    /**
-     * @return \Doctrine\ORM\QueryBuilder
-     */
-    private function getQueryBuilder()
-    {
-        $em = $this->getDoctrine()->getEntityManager();
-        $qb = $em->createQueryBuilder();
-
-        return $qb;
-    }
-
-    /**
-     * @return array|\Phones\PhoneBundle\Entity\Phone[]
-     */
-    private function getPhones()
-    {
-        $products = $this->getDoctrine()
-            ->getRepository('PhonesPhoneBundle:Phone')
-            ->findAll();
-
-        return $products;
-    }
-
-    private function getExistingOs()
-    {
-        /** @var EntityManager $em */
-        $em = $this->getDoctrine()->getRepository('PhonesPhoneBundle:Phone');
-        $qb = $em->createQueryBuilder('a')->groupBy('a.os');
-        $result = $qb->getQuery()->getResult();
-
-        $distinctOs = [];
-        /** @var Phone $os */
-        foreach($result as $os) {
-            $distinctOs[] = $os->getOs();
-        }
-
-        return $distinctOs;
-    }
-
-    private function getExistingBrands()
-    {
-        $qb = $this->getDoctrine()->getRepository('PhonesPhoneBundle:Phone')->createQueryBuilder('a')->groupBy('a.brand');
-        $result = $qb->getQuery()->getResult();
-
-        $distinct = [];
-        /** @var Phone $os */
-        foreach($result as $os) {
-            $distinct[] = $os->getBrand();
-        }
-
-        return $distinct;
     }
 }
---
+++ src/Phones/FrontEndBundle/Service/QueryHelper.php
@@ -0,0 +1,249 @@
+<?php
+
+namespace Phones\FrontEndBundle\Service;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query\Expr;
+use Phones\PhoneBundle\Entity\Phone;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class QueryHelper extends Controller
+{
+    /** @var EntityManager */
+    private $entityManager;
+
+    /** @var array  */
+    private $possibleRangeNames = [];
+
+    /** @var array  */
+    private $possibleSelectNames = [];
+
+    /** @var array  */
+    private $possibleLikeSelectNames = [];
+
+    /** @var array  */
+    private $possibleCheckBoxNames = [];
+
+    /**
+     * @param EntityManager $entityManager
+     */
+    public function setEntityManager($entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param array $possibleCheckBoxNames
+     */
+    public function setPossibleCheckBoxNames($possibleCheckBoxNames)
+    {
+        $this->possibleCheckBoxNames = $possibleCheckBoxNames;
+    }
+
+    /**
+     * @param array $possibleLikeSelectNames
+     */
+    public function setPossibleLikeSelectNames($possibleLikeSelectNames)
+    {
+        $this->possibleLikeSelectNames = $possibleLikeSelectNames;
+    }
+
+    /**
+     * @param array $possibleRangeNames
+     */
+    public function setPossibleRangeNames($possibleRangeNames)
+    {
+        $this->possibleRangeNames = $possibleRangeNames;
+    }
+
+    /**
+     * @param array $possibleSelectNames
+     */
+    public function setPossibleSelectNames($possibleSelectNames)
+    {
+        $this->possibleSelectNames = $possibleSelectNames;
+    }
+
+    public function getBestPhones(Request $request){
+        $qBuilder = $this->getQueryBuilder();
+
+        $qBuilder->addSelect('phones.phoneId');
+        $qBuilder->addSelect('costs.cost');
+        $qBuilder->addSelect('SUM(stats.grade) AS points');
+
+        $qBuilder->from('PhonesPhoneBundle:Phone','phones');
+
+        $qBuilder->leftJoin('PhonesPhoneBundle:Cost', 'costs', 'WITH', 'costs.phone_id=phones.phoneId');
+        $qBuilder->leftJoin('PhonesPhoneBundle:Stat', 'stats', 'WITH', 'stats.phone_id=phones.phoneId');
+
+        $qBuilder->addGroupBy('phones.phoneId');
+//        $qBuilder->addGroupBy('points');
+//        $qBuilder->set('stats.grade', $qBuilder->expr()->sum('stats.grade', '2'));
+//        $qBuilder->set('stats.grade', $qBuilder->expr()->countDistinct('stats.grade'));
+
+        $qBuilder->addOrderBy('points', 'DESC');
+        $qBuilder->addOrderBy('costs.cost', 'ASC');
+
+        $queryParams = $request->query->getIterator();
+        foreach ($queryParams as $paramName => $paramValue) {
+            if (isset($this->possibleRangeNames[$paramName])) {
+                $params = explode(',', $paramValue);
+                $column = sprintf('%s.%s', $this->possibleRangeNames[$paramName], $paramName);
+                $this->generateRangeConditions(
+                    $qBuilder,
+                    $column . ' >= :%s AND '. $column . ' <= :%s',
+                    $paramName,
+                    $params
+                );
+            } elseif (isset($this->possibleSelectNames[$paramName])) {
+                if (is_array($paramValue)) {
+                    if (in_array('any', $paramValue)) {
+                        unset($paramValue[array_search('any', $paramValue)]);
+                    }
+                    $column = sprintf('%s.%s', $this->possibleSelectNames[$paramName], $paramName);
+                    $this->generateMultipleConditions(
+                        $qBuilder,
+                        $qBuilder->expr()->orX(),
+                        $column . ' = :%s',
+                        $paramName,
+                        $paramValue
+                    );
+                }
+            } elseif (isset($this->possibleLikeSelectNames[$paramName]))  {
+                if (is_array($paramValue)) {
+                    if (in_array('any', $paramValue)) {
+                        unset($paramValue[array_search('any', $paramValue)]);
+                    }
+
+                    $column = $this->possibleLikeSelectNames[$paramName] . '.' . $paramName;
+                    foreach ($paramValue as $key => $valueStr) {
+                        //need protection
+                        $paramValue[$key] = '%' . $valueStr . '%';
+                    }
+                    $this->generateMultipleConditions(
+                        $qBuilder,
+                        $qBuilder->expr()->orX(),
+                        $column . ' LIKE :%s',
+                        $paramName,
+                        $paramValue
+                    );
+                }
+            } elseif (isset($this->possibleCheckBoxNames[$paramName])) {
+                $column = $this->possibleCheckBoxNames[$paramName] . '.' . $paramName;
+                if (!empty($paramValue)) {
+                    $this->generateMultipleConditions(
+                        $qBuilder,
+                        $qBuilder->expr()->orX(),
+                        $column . ' = :%s',
+                        $paramName,
+                        [1]
+                    );
+                }
+            }
+        }
+
+        $query = $qBuilder->getQuery();
+        $result = $query->getResult();
+        //var_export($query->getDQL());
+        //var_dump($query->getResult());
+
+        return $result;
+    }
+
+    /**
+     * @param \Doctrine\ORM\QueryBuilder $qBuilder
+     * @param Expr $expr
+     * @param string $pattern
+     * @param string $fieldName
+     * @param array $values
+     */
+    private function generateMultipleConditions($qBuilder, $expr, $pattern, $fieldName, $values)
+    {
+        foreach ($values as $key => $value) {
+            //need protection from SLQ injection
+            $paramName = $fieldName . $key;
+            $condition = sprintf($pattern, $paramName);
+            $expr->add($condition);
+            $qBuilder->setParameter($paramName, $value);
+        }
+
+        $qBuilder->andWhere($expr);
+    }
+
+    /**
+     * @param \Doctrine\ORM\QueryBuilder $qBuilder
+     * @param string $pattern
+     * @param string $fieldName
+     * @param array  $values
+     */
+    private function generateRangeConditions($qBuilder, $pattern, $fieldName, $values)
+    {
+        $fromVal = isset($values[0]) && is_numeric($values[0]) ? $values[0] : null;
+        $toVal   = isset($values[1]) && is_numeric($values[1]) ? $values[1] : null;
+        if ($fromVal != null && $toVal != null) {
+            $paramFromName = $fieldName. 0;
+            $paramToName   = $fieldName. 1;
+            $qBuilder->setParameter($paramFromName, $fromVal);
+            $qBuilder->setParameter($paramToName, $toVal);
+
+            $condition = sprintf($pattern, $paramFromName, $paramToName);
+            $qBuilder->andWhere($condition);
+        }
+    }
+
+    /**
+     * @return \Doctrine\ORM\QueryBuilder
+     */
+    private function getQueryBuilder()
+    {
+        $em = $this->entityManager;
+        $qb = $em->createQueryBuilder();
+
+        return $qb;
+    }
+
+    /**
+     * @return array|\Phones\PhoneBundle\Entity\Phone[]
+     */
+    public function getPhones()
+    {
+        $products = $this->entityManager
+            ->getRepository('PhonesPhoneBundle:Phone')
+            ->findAll();
+
+        return $products;
+    }
+
+    public function getExistingOs()
+    {
+        /** @var EntityManager $em */
+        $em = $this->entityManager->getRepository('PhonesPhoneBundle:Phone');
+        $qb = $em->createQueryBuilder('a')->groupBy('a.os');
+        $result = $qb->getQuery()->getResult();
+
+        $distinctOs = [];
+        /** @var Phone $os */
+        foreach($result as $os) {
+            $distinctOs[] = $os->getOs();
+        }
+
+        return $distinctOs;
+    }
+
+    public function getExistingBrands()
+    {
+        $qb = $this->entityManager->getRepository('PhonesPhoneBundle:Phone')->createQueryBuilder('a')->groupBy('a.brand');
+        $result = $qb->getQuery()->getResult();
+
+        $distinct = [];
+        /** @var Phone $os */
+        foreach($result as $os) {
+            $distinct[] = $os->getBrand();
+        }
+
+        return $distinct;
+    }
+}
