715c34597009b833b7df05e66e60c80bbe60a02a
Mazvis <sivzam@gmail.com>
2015-04-05 20:48:39 +0300

    good crap, I like it

--- src/Phones/FrontEndBundle/Service/QueryHelper.php
+++ src/Phones/FrontEndBundle/Service/QueryHelper.php
@@ -2,8 +2,10 @@
 
 namespace Phones\FrontEndBundle\Service;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Query\Expr;
+use Doctrine\ORM\Query\Parameter;
 use Doctrine\ORM\QueryBuilder;
 use Phones\PhoneBundle\Entity\Phone;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,6 +14,9 @@ class QueryHelper
 {
     /** @var EntityManager */
     private $entityManager;
+
+    /** @var  Connection */
+    private $dbConnection;
 
     /** @var array  */
     private $possibleRangeNames = [];
@@ -34,6 +39,14 @@ class QueryHelper
     public function setEntityManager($entityManager)
     {
         $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param Connection $dbConnection
+     */
+    public function setDbConnection($dbConnection)
+    {
+        $this->dbConnection = $dbConnection;
     }
 
     /**
@@ -82,12 +95,50 @@ class QueryHelper
         $this->setFormValues($request, $qBuilder);
 
         $query = $qBuilder->getQuery();
-        $result = $query->getResult();
+        //#1
+//        $result = $query->getResult();
 
         var_export($query->getDQL());
-//        var_dump($query->getResult());
 
-        return $result;
+        /** @var Parameter $parameterData */
+        $paramArray = [];
+        foreach ($query->getParameters() as $parameterData) {
+            $paramArray[$parameterData->getName()] = $parameterData->getValue();
+        }
+
+        //#2 convert queryBuilder to createQuery
+//        $em = $this->entityManager;
+//        $query = $em->createQuery($query->getDQL());
+//        $query->setParameters($paramArray);
+//        $result = $query->getResult();
+
+        //#3 execute with dbConnection
+        $queryStr = $query->getDQL();
+        var_dump('bandom');
+        var_export($queryStr);
+        $queryStr = str_replace('phones', 'phone', $queryStr);
+        $queryStr = str_replace('costs', 'cost', $queryStr);
+        $queryStr = str_replace('audio_output', 'stat_audio_output', $queryStr);
+        $queryStr = str_replace('battery_charging_times', 'stat_battery_charging_times', $queryStr);
+//        $queryStr = str_replace('cost_original', 'cost', $queryStr);
+        $queryStr = str_replace('WITH', 'ON', $queryStr);
+        $pregResult = preg_replace('/[a-z]+:[a-z]+/i', '', $queryStr);
+        if ($pregResult) {
+            $queryStr = $pregResult;
+        }
+        foreach ($paramArray as $name => $value) {
+            $queryStr = str_replace(':'.$name, $value, $queryStr);
+        }
+
+        var_dump('po');
+        var_export($queryStr);
+        $regeneratedQuery = '';
+//        $stmt = $this->dbConnection->executeQuery($queryStr);
+        $stmt = $this->dbConnection->executeQuery('SELECT phone.phoneId, minPrice, (COALESCE(SUM(stat_audio_output.grade),0)+COALESCE(SUM(stat_battery_charging_times.grade),0)) AS points FROM phone LEFT JOIN (SELECT MIN(cost.cost) AS minPrice, cost.phone_id FROM cost GROUP BY cost.phone_id) AS costs ON costs.phone_id=phone.phoneId LEFT JOIN stat_audio_output ON stat_audio_output.phoneId=phone.phoneId LEFT JOIN stat_battery_charging_times ON stat_battery_charging_times.phoneId=phone.phoneId WHERE (minPrice >= 250 AND minPrice <= 1250) AND (phone.weight >= 0 AND phone.weight <= 200) AND (phone.cpu_freq >= 500 AND phone.cpu_freq <= 3000) AND (phone.cpu_cores >= 1 AND phone.cpu_cores <= 8) AND (phone.ram_mb >= 0 AND phone.ram_mb <= 3072) AND (phone.display_size >= 0 AND phone.display_size <= 6) AND (phone.camera_mpx >= 0 AND phone.camera_mpx <= 41) AND (phone.video_p >= 0 AND phone.video_p <= 2160) AND (phone.battery_talk_time >= 4 AND phone.battery_talk_time <= 20) GROUP BY phone.phoneId ORDER BY points DESC, minPrice ASC');
+        $rez = $stmt->fetchAll();
+        var_dump($rez);
+//        die;
+//        return $result;
     }
 
     /**
@@ -114,12 +165,9 @@ class QueryHelper
         $qBuilder->addSelect('phones.phoneId');
         $qBuilder->addSelect('costs.cost');
 
-        $qBuilder->from('PhonesPhoneBundle:Phone','phones');
-
-        $qBuilder->leftJoin('PhonesPhoneBundle:Cost', 'costs', 'WITH', 'costs.phone_id=phones.phoneId');
-//        $qBuilder->expr()->any('LEFT JOIN ('.$qBuilder2->getDQL(). ') AS costs WITH costs.phone_id=phones.phoneId');
-//        $qBuilder->leftJoin( $qBuilder2->getDQL(). ' WITH costs.phone_id=phones.phoneId', 'costs');
-//        $qBuilder->leftJoin($qBuilder2->getDQL(), 'costs', 'WITH', 'costs.phone_id=phones.phoneId');
+//        $qBuilder->from('PhonesPhoneBundle:Phone','phones');
+//        $qBuilder->leftJoin('PhonesPhoneBundle:Cost', 'costs', 'WITH', 'costs.phone_id=phones.phoneId');
+        $qBuilder->from('PhonesPhoneBundle:Phone','phones'. ' LEFT JOIN ('.$qBuilder2->getDQL().') AS costs WITH costs.phone_id=phones.phoneId');
 
         $qBuilder->addGroupBy('phones.phoneId');
 
